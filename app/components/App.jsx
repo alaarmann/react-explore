9d3afdc6f39162f2d15d48d12dc84aa2c45963a6
Alexander Laarmann <alaarmann@gmx.net>
2016-11-24 22:01:47 +0100

    Add deleting and editing

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -41,7 +41,14 @@ export default class App extends React.Component {
         
         <button onClick={this.addNote}>+</button>
 
-        <Notes notes={notes} />
+        
+        <Notes
+          notes={notes}
+          onNoteClick={this.activateNoteEdit}
+          onEdit={this.editNote}
+          onDelete={this.deleteNote}
+          />
+
       </div>
     );
   }
@@ -63,4 +70,37 @@ export default class App extends React.Component {
       }])
     });
   }
+
+  deleteNote = (id, e) => {
+    // Avoid bubbling to edit
+    e.stopPropagation();
+
+    this.setState({
+      notes: this.state.notes.filter(note => note.id !== id)
+    });
+  }
+  activateNoteEdit = (id) => {
+    this.setState({
+      notes: this.state.notes.map(note => {
+        if(note.id === id) {
+          note.editing = true;
+        }
+
+        return note;
+      })
+    });
+  }
+  editNote = (id, task) => {
+    this.setState({
+      notes: this.state.notes.map(note => {
+        if(note.id === id) {
+          note.editing = false;
+          note.task = task;
+        }
+
+        return note;
+      })
+    });
+  }
+
 }
--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 
-export default ({notes}) => (
-  <ul>{notes.map(note =>
-    <li key={note.id}>{note.task}</li>
-  )}</ul>
-)
+import Note from './Note';
 
+import Editable from './Editable';
+
+
+export default ({
+  notes,
+  onNoteClick=() => {}, onEdit=() => {}, onDelete=() => {}
+}) => (
+  <ul>{notes.map(({id, editing, task}) =>
+    <li key={id}>
+      <Note onClick={onNoteClick.bind(null, id)}>
+        <Editable
+           editing={editing}
+           value={task}
+           onEdit={onEdit.bind(null, id)} />
+        <button onClick={onDelete.bind(null, id)}>x</button>
+      </Note>
+    </li>
+  )}</ul>
+
+)
