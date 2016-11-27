62fa03b08140ab44e3c927fba080bdf19195c901
Alexander Laarmann <alaarmann@gmx.net>
2016-11-27 21:02:14 +0100

    Add persistence to localStorage

--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -8,16 +8,8 @@ export default class NoteStore {
 
     this.bindActions(NoteActions);
 
-    this.notes = [
-      {
-        id: uuid.v4(),
-        task: 'Learn React'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Do laundry'
-      }
-    ];
+    this.notes = [];
+
   }
 
 
