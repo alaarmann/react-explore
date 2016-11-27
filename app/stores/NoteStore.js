74548503e30e44d0f8418e7ae55903f8399cfc0c
Alexander Laarmann <alaarmann@gmx.net>
2016-11-27 20:33:21 +0100

    Implement actions

--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -1,7 +1,13 @@
 import uuid from 'uuid';
 
+import NoteActions from '../actions/NoteActions';
+
+
 export default class NoteStore {
   constructor() {
+
+    this.bindActions(NoteActions);
+
     this.notes = [
       {
         id: uuid.v4(),
@@ -13,4 +19,34 @@ export default class NoteStore {
       }
     ];
   }
+
+
+  create(note) {
+
+    this.setState({
+      notes: this.notes.concat(note)
+    });
+
+  }
+  update(updatedNote) {
+
+    this.setState({
+      notes: this.notes.map(note => {
+        if(note.id === updatedNote.id) {
+          return Object.assign({}, note, updatedNote);
+        }
+
+        return note;
+      })
+    });
+
+  }
+  delete(id) {
+    this.setState({
+      notes: this.notes.filter(note => note.id !== id)
+    });
+
+  }
+
+
 }
